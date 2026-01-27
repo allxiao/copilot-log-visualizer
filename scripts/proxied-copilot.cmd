95b8d8d8ecd241ffd9de87fb62ce58e594e35579
allxiao <arieshout@gmail.com>
2026-01-27 17:03:05 +0800

    update start copilot script

--- scripts/proxied-copilot.cmd
+++ scripts/proxied-copilot.cmd
@@ -1,8 +1,9 @@
 @echo off
 setlocal
 
-set http_proxy=http://127.0.0.1:8080
-set https_proxy=http://127.0.0.1:8080
+set HTTP_PROXY=http://127.0.0.1:8080
+set HTTPS_PROXY=http://127.0.0.1:8080
+set NODE_USE_ENV_PROXY=1
 set NODE_TLS_REJECT_UNAUTHORIZED=0
 
-copilot %*
+call copilot %*
--- scripts/proxied-copilot.sh
+++ scripts/proxied-copilot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export http_proxy=http://127.0.0.1:8080
-export https_proxy=http://127.0.0.1:8080
+export HTTP_PROXY=http://127.0.0.1:8080
+export HTTPS_PROXY=http://127.0.0.1:8080
+export NODE_USE_ENV_PROXY=1
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
 copilot "$@"
